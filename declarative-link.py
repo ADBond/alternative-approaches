3b3ac87535c2a125e3daa1c6c194f0aa3eb8d216
ADBond <48208438+ADBond@users.noreply.github.com>
2023-11-01 10:39:28 +0000

    model parameters + comparison level config in spec

--- declarative-link.py
+++ declarative-link.py
@@ -1,6 +1,10 @@
 from splink.config import linker_from_config
 
-linker = linker_from_config("./splink-decl.yaml", format="yaml")
+linker = linker_from_config(
+    "./splink-decl.yaml",
+    format="yaml",
+    keep_existing_parameter_estimates=True,
+)
 # optional step
 # linker.run_training()
 linker.predict()
