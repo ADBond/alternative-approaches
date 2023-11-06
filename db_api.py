45b53112554a1062be27c36e681ca6a2e6dc6702
ADBond <48208438+ADBond@users.noreply.github.com>
2023-11-06 16:38:09 +0000

    overloads and tooling help

--- db_api.py
+++ db_api.py
@@ -1,33 +1,55 @@
+from typing import overload, Literal, Type
+
 class DatabaseAPI:
 
     def __init__(self, *args, **kwargs):
         print(f"Creating type {type(self)} with {args} and {kwargs}")
 
     @classmethod
-    def from_str(cls, string, *args, **kwargs):
+    def subclass_from_str(cls, string):
         # use sequence unpacking form as will catch if we duplicate
         # _name_for_factory in subclasses
         [subclass] = (
             c for c in cls.__subclasses__()
             if c._name_for_factory == string
         )
-        return subclass(*args, **kwargs)
-
-# user-facing:
-def db_api(backend_str, *args, **kwargs):
-    return DatabaseAPI.from_str(backend_str, *args, **kwargs)
+        return subclass
 
 class DuckDBAPI(DatabaseAPI):
+    """num tum etc explanations for Duckdb"""
     _name_for_factory = "duckdb"
+    def __init__(self, num, tum, thing=None):
+        print(f"{num=}, {tum=}, {thing=}")
 
 class SparkAPI(DatabaseAPI):
+    """num tum rum etc explanations for Spark"""
     _name_for_factory = "spark"
+    def __init__(self, num, tum, rum, thing=None, spark="spork"):
+        print(f"{num=}, {tum=}, {rum=}, {thing=}")
+
+@overload
+def db_api(backend_str: Literal["duckdb"]) -> Type[DuckDBAPI]:
+    ...
+
+@overload
+def db_api(backend_str: Literal["spark"]) -> Type[SparkAPI]:
+    ...
+
+# user-facing:
+def db_api(backend_str: str) -> Type[DatabaseAPI]:
+    """docstring for db_api"""
+    return DatabaseAPI.subclass_from_str(backend_str)
 
 
 if __name__ == "__main__":
-    db_api("duckdb", 3, 4, thing="spling")
-    db_api("spark", 5, 4, 2, thing="splingo", spark="spark")
+    DuckDBAPI(3, 1, thing=123)
+    db_api("duckdb")(3, 4, thing="spling")
+    db_api("spark")(5, 4, 2, thing="splingo", spark="spark")
     try:
-        db_api("invalid_backend", 5, 4, 2, thing="splingo", spark="spark")
+        db_api("invalid_backend")(5, 4, 2, thing="splingo", spark="spark")
     except ValueError as e:
         print(e)
+    # try out tooltips:
+    # sp = db_api("spark")
+    # sp = db_api("spark")()
+    # dk = db_api("duckdb")()
